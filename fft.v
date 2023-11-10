76861cf0dfb0a9e8bdf6b3d7a7d8bc11714c8926
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2023-11-10 11:42:30 +0530

    Code with stage 2 working and 4 points in stage 3
    In this code, stage 2 has been implemented. And 4 points of stage 3 have also been implemented.

--- fft.v
+++ fft.v
@@ -28,11 +28,13 @@ module main(
   input [15:0] in6_real, input [15:0] in6_imag,
   input [15:0] in7_real, input [15:0] in7_imag
 );
-  wire [15:0] g_real [0:7];   // Declare g_real as an array of registers to store stage 0 results
-  wire [15:0] g_imag [0:7];   // Declare g_imag as an array of registers
-  wire [15:0] h_real [0:7];
-  wire [15:0] h_imag [0:7];
-        
+  wire [15:0] g_real [0:7];   // Declare g_real as an array of registers to store real part of stage 1 results
+  wire [15:0] g_imag [0:7];   // Declare g_imag as an array of registers to store imag part of stage 1 results
+  wire [15:0] h_real [0:7];   // Declare h_real as an array of registers to store real part of stage 2 results
+  wire [15:0] h_imag [0:7];   // Declare h_imag as an array of registers to store imag part of stage 2 results
+  wire [15:0] i_real [0:7];   // Declare i_real as an array of registers to store real part of stage 3 results
+  wire [15:0] i_imag [0:7];   // Declare i_imag as an array of registers to store imag part of stage 3 results
+         
   stage1 inst_g0_real (.x(in0_real) , .y(in4_real) , .flag(1) , .z(g_real[0]));    // g0_real = in0_real + in4_real
   stage1 inst_g0_imag (.x(in0_imag) , .y(in4_imag) , .flag(1) , .z(g_imag[0]));    // g0_imag = in0_imag + in4_imag
   stage1 inst_g1_real (.x(in0_real) , .y(in4_real) , .flag(0) , .z(g_real[1]));    // g1_real = in0_real - in4_real
@@ -67,4 +69,14 @@ module main(
   stage1 inst_h6_imag (.x(g_imag[4]), .y(g_imag[6]),    .flag(0), .z(h_imag[6]));  // h6_imag = g4_imag - g6_imag
   stage1 inst_h7_real (.x(g_real[5]), .y(-(g_imag[7])), .flag(1), .z(h_real[7]));  // h7_real = g5_real + (j*g7_real)
   stage1 inst_h7_imag (.x(g_imag[5]), .y(g_real[7]),    .flag(1), .z(h_imag[7]));  // h7_imag = g5_imag + (j*g7_imag)
+
+  stage1 inst_i0_real (.x(h_real[0]), .y(h_real[4]),    .flag(1), .z(i_real[0]));  // i0_real = h0_real + h4_real
+  stage1 inst_i0_imag (.x(h_imag[0]), .y(h_imag[4]),    .flag(1), .z(i_imag[0]));  // i0_imag = h0_imag + h4_imag
+  stage1 inst_i2_real (.x(h_real[2]), .y(-(h_imag[6])), .flag(0), .z(i_real[2]));  // i2_real = h2_real - (j*h6_real)
+  stage1 inst_i2_imag (.x(h_imag[2]), .y(h_real[6]),    .flag(0), .z(i_imag[2]));  // i2_imag = h2_imag - (j*h6_imag) 
+  stage1 inst_i4_real (.x(h_real[0]), .y(h_real[4]),    .flag(0), .z(i_real[4]));  // i4_real = h0_real - h4_real
+  stage1 inst_i4_imag (.x(h_imag[0]), .y(h_imag[4]),    .flag(0), .z(i_imag[4]));  // i4_imag = h0_imag - h4_imag
+  stage1 inst_i6_real (.x(h_real[2]), .y(-(h_imag[6])), .flag(1), .z(i_real[6]));  // i6_real = h2_real + (j*h6_real)
+  stage1 inst_i6_imag (.x(h_imag[2]), .y(h_real[6]),    .flag(1), .z(i_imag[6]));  // i6_imag = h2_imag + (j*h6_imag)
+
 endmodule
